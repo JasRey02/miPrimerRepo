885e1e901e45b470d2b8cb80a5235893e4ba1e65
Jas Rey <jasrey@hotmail.es>
2026-04-30 15:24:48 -0600

    otras modificaciones en program.cs

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
-string stripe_api_key = "sk_test_51N8m9uKZsQh3nqj2l7Xo5v1a2b3c4d5e6f7g8h9i4x5y6z7a8b9c";
+Console.WriteLine("Hello, World!");
